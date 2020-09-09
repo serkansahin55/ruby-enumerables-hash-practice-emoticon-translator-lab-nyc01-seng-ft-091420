c5f2c0ff0bfa2c15c34bd4a5b748ae1dd83a1952
Serkan Sahin <ssahin55@outlook.com>
2020-09-09 14:48:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,30 @@
 # require modules here
-
-def load_library
+require 'yaml'
+require 'pry'
+def load_library(path)
   # code goes here
+  yaml = YAML.load_file(path)
+  emoticons = {}
+  yaml.each do |key, value|
+    emoticons[key] = {english: value[0], japanese: value[1]}
+  end
+  emoticons
 end
 
 def get_japanese_emoticon
   # code goes here
+  
 end
 
 def get_english_meaning
   # code goes here
 end
+
+
+# yaml = YAML.load_file(path)
+#   emoticons = {}
+#   yaml.each { |key, value|
+#     emoticons[key] = { english: value[0], japanese: value[1] }
+#   }
+#   emoticons
+
